65e35f371e6c6752151da4dd7670a04ba0233a00
matthew010505 <matthewmelritchie2005@gmail.com>
2024-08-02 18:33:37 +0530

    bug fix

--- src/Carousel.jsx
+++ src/Carousel.jsx
@@ -22,17 +22,17 @@ const Carousel = () => {
       <div className="max-w-2xl mx-auto">
         <div id="default-carousel" className="relative" data-carousel="static">
           <div className="overflow-hidden relative h-56 rounded-lg sm:h-64 xl:h-80 2xl:h-96">
-            <div className="active duration-700 ease-in-out" data-carousel-item>
+            <div className="active duration-700 ease-in-out" data-carousel-item="active">
               <span className="absolute top-1/2 left-1/2 text-2xl font-semibold text-white -translate-x-1/2 -translate-y-1/2 sm:text-3xl dark:text-gray-800">First Slide</span>
               <img src={work1} className="block absolute top-1/2 left-1/2 w-full -translate-x-1/2 -translate-y-1/2" alt="First Slide" />
             </div>
-            <div className="active duration-700 ease-in-out" data-carousel-item>
+            <div className="duration-700 ease-in-out" data-carousel-item>
               <img src={work2} className="block absolute top-1/2 left-1/2 w-full -translate-x-1/2 -translate-y-1/2" alt="Second Slide" />
             </div>
-            <div className="active duration-700 ease-in-out" data-carousel-item>
+            <div className="duration-700 ease-in-out" data-carousel-item>
               <img src={work3} className="block absolute top-1/2 left-1/2 w-full -translate-x-1/2 -translate-y-1/2" alt="Third Slide" />
             </div>
-            <div className="active duration-700 ease-in-out" data-carousel-item>
+            <div className="duration-700 ease-in-out" data-carousel-item>
               <img src={work4} className="block absolute top-1/2 left-1/2 w-full -translate-x-1/2 -translate-y-1/2" alt="Fourth Slide" />
             </div>
           </div>
@@ -40,6 +40,7 @@ const Carousel = () => {
             <button type="button" className="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 1" data-carousel-slide-to="0"></button>
             <button type="button" className="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 2" data-carousel-slide-to="1"></button>
             <button type="button" className="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 3" data-carousel-slide-to="2"></button>
+            <button type="button" className="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 4" data-carousel-slide-to="3"></button>
           </div>
           <button type="button" className="flex absolute top-0 left-0 z-30 justify-center items-center px-4 h-full cursor-pointer group focus:outline-none" data-carousel-prev>
             <span className="inline-flex justify-center items-center w-8 h-8 rounded-full sm:w-10 sm:h-10 bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
@@ -54,8 +55,6 @@ const Carousel = () => {
             </span>
           </button>
         </div>
-
-        
       </div>
     </div>
   )

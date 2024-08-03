86319ad135c3042308df503e3c5e0a6d1061f19e
matthew010505 <matthewmelritchie2005@gmail.com>
2024-08-03 20:50:11 +0530

    carousel fix

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import Tuto from './Tuto.jsx'
 import Header from './Header.jsx'
 import Carousel from './Carousel.jsx'
 
@@ -10,7 +9,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <div >
     <Header/>
-    <Carousel/>
+    <App/>
     </div>
     
   </React.StrictMode>,
